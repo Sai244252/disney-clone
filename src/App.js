bf676fb597dca6e50d0e7106e7a104d1fa57eef8
Sai Raj <sai244252@gmail.com>
2024-03-03 00:13:33 +0530

    B5-redux-login-logout-functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
